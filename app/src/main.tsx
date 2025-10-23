029f93a512dfad1d36f052eac6ce138f0f29e00a
Summer Higgins <summerrhiggins@gmail.com>
2025-10-23 05:46:58 -0400

    chore(app): wire DeviceContext, router shell, and CSS entry so styles load

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,13 +1,16 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";        
 import { BrowserRouter } from "react-router-dom";
-import App from "./App.tsx";
-import "./index.css";
+import App from "./App";
+import { DeviceProvider } from "./state/DeviceContext";
+import "./index.css";                      
 
-createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <DeviceProvider>
+        <App />
+      </DeviceProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
