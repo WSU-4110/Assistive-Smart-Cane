508ead57aa115e0d93852267220bb70a86497e38
adamwehbi <Adamwehbi0@gmail.com>
2025-11-05 10:56:05 -0500

    Set up Jest testing framework and configuration

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src', '<rootDir>/services', '<rootDir>/components'],
+  testMatch: [
+    '**/__tests__/**/*.test.(ts|tsx)',
+    '**/?(*.)+(spec|test).(ts|tsx)'
+  ],
+  collectCoverageFrom: [
+    'services/**/*.{ts,tsx}',
+    'components/**/*.{ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**'
+  ],
+  coverageDirectory: 'coverage',
+  verbose: true
+};
